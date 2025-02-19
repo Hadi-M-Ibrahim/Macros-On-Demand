80bb1a0ae35c9e84d969a3413a6f315df9a1a437
lwu1822 <lwu1822@gmail.com>
2025-02-19 00:47:56 -0800

    add signup screen design; implement button response when clicked

--- Frontend/App.js
+++ Frontend/App.js
@@ -24,7 +24,7 @@ const App = () => {
           >
             <Stack.Screen
               name="SignUp"
-              component={LoginScreen}
+              component={SignUpScreen}
               options={{ headerShown: false }}
             />
             <Stack.Screen
--- Frontend/screens/SignUpScreen.js
+++ Frontend/screens/SignUpScreen.js
@@ -1,14 +1,85 @@
 import React, { useState } from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, TouchableOpacity, StyleSheet } from "react-native";
 import { Button, Input, Stack, Text, YStack } from "tamagui";
 
 const SignUpScreen = ({ navigation }) => {
 
-  };
-
   return (
-    
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "pink",
+      }}
+    >
+
+      <YStack
+        space="$4"
+        padding="$4"
+        borderRadius="$4"
+        backgroundColor="$color2"
+        alignItems="center"
+        width={500}
+      >
+        <Text
+          fontSize="$6"
+          fontWeight="bold"
+          color="#4A2040"
+          textAlign="center"
+          fontFamily="$heading"
+        >
+          Sign Up 
+        </Text>
+
+        <Stack width="100%">
+          <Text>Email</Text>
+          <Input
+            placeholder="Enter your email: "
+            padding="$2"
+          />
+        </Stack>
+
+        <Stack width="100%">
+          <Text>Password</Text>
+          <Input
+            placeholder="Enter your password: "
+            secureTextEntry
+            padding="$2"
+          />
+        </Stack>
+
+        <Stack width="100%">
+          <Text>Confirm Password</Text>
+          <Input
+            placeholder="Enter your password: "
+            secureTextEntry
+            padding="$2"
+          />
+        </Stack>
+
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.25}
+        >
+          Create Account
+        </TouchableOpacity>
+
+      </YStack>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#9F6BA0",
+    padding: 16,
+    width: "100%",
+    borderRadius: 16,
+    marginTop: 16,
+    alignItems: "center"
+  },
+});
+
 
 export default SignUpScreen;

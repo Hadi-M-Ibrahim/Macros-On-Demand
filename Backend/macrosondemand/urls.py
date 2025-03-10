f4b91ef15241c17bfc573690d62a22c4758e33e0
Jeicex1 <99057897+Jeicex1@users.noreply.github.com>
2025-03-10 14:07:12 -0700

    fix typo signin to signup

--- Backend/macrosondemand/urls.py
+++ Backend/macrosondemand/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from django.shortcuts import redirect
 
 def home_redirect(request):
-    return redirect('/api/auth/signin/')  # Redirect to the sign-in page
+    return redirect('/api/auth/signup/')  # Redirect to the sign-in page
 
 urlpatterns = [
     path('', home_redirect, name='home-redirect'),  # Root URL redirects to sign-in

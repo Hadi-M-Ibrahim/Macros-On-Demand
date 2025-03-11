af82c555bd1aaf9502e961ed9db72c0e13cb06b9
Jonathan Kim <jonathankim1626@gmail.com>
2025-03-11 15:15:38 -0700

    Add URL pattern to main URL configurations

--- Backend/macrosondemand/urls.py
+++ Backend/macrosondemand/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('api/auth/', include('apps.accounts.urls')),
     path('api/search/meal-options/', meal_options_view, name='meal-options'),
     path('api/search/save-meal/', save_meal_view, name='save-meal'),
+    path('api/search/ranked-meals/', ranked_meal_options_view, name='ranked-meals'),
 ]
